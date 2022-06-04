ed77a27328ff242390d32c005717b4dfd90b5646
Pawel Pudlo <pawelzlatan98@interia.pl>
2022-06-04 16:23:42 +0200

    first commit

---
+++ src/main/java/com/example/footbalapp/FootbalappApplication.java
@@ -0,0 +1,13 @@
+package com.example.footbalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootbalappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootbalappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/footbalapp/FootbalappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footbalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootbalappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
